3068395700d2559c48cfa51422179d53cfb3d1a2
nehaswaroop <neha.thakur.swaroop@gmail.com>
2023-01-03 15:27:57 -0800

    Create index.js

---
+++ class/javascript/index.js
@@ -0,0 +1,30 @@
+var myName = "Neha";
+let age = 10;
+const email = "neha.thakur.swaroop@gmail.com";
+var active = true;
+let membership;
+const date_created = null;
+
+console.log(nameA);
+var nameA = "Neha";
+add(5, 10);
+function add(a, b) {
+	console.log(a + b);
+}
+const subtract = function (a, b) {
+	console.log(a - b);
+};
+subtract(15, 8);
+
+const multiply = (a, b) => {
+	console.log(a * b);
+};
+multiply(5, 4);
+
+console.log("begin running");
+function addi(a, b) {
+	console.log(a + b);
+	return a + b;
+}
+console.log(addi(8, 7));
+console.log("done");
